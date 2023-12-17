f48c840d1bb7aadfda0b8f7675386b258154f701
smruti <smrutishreemishra03@gmail.com>
2023-12-17 14:14:14 +0530

    2ndcommit

--- src/demo3/demo_class.java
+++ src/demo3/demo_class.java
@@ -5,6 +5,11 @@ public class demo_class {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Lord smruti tester sdet");
+		System.out.println(multiplication(10,20));
+	}
+	public static int multiplication(int a,int b) {
+		return a*b;
+		
 	}
 
 }
