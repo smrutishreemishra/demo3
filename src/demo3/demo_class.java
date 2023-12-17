480a8394a1ac71fca58f3444ab350ea1484c8225
smruti <smrutishreemishra03@gmail.com>
2023-12-17 13:53:39 +0530

    1stcommit

---
+++ src/demo3/demo_class.java
@@ -0,0 +1,10 @@
+package demo3;
+
+public class demo_class {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Lord smruti tester sdet");
+	}
+
+}
