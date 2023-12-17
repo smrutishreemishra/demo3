9e2c4cba0452f85da8737a3489969368452f0429
MishraSomi <somi94m@gmail.com>
2023-12-17 14:14:51 +0530

    sub Functionality added

--- src/demo3/demo_class.java
+++ src/demo3/demo_class.java
@@ -5,6 +5,10 @@ public class demo_class {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Lord smruti tester sdet");
+		
+		int a=30;
+		int b= 10;
+		System.out.println(a-b);
 	}
 
 }
